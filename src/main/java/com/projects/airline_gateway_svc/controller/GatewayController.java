35c89a2c0c4ea465e8d2dcdb507bf7962ca230a2
Omkar <omkarindalkar.9@gmail.com>
2025-08-05 23:35:00 +0530

    Omkar|Initial commit

---
+++ src/main/java/com/projects/airline_gateway_svc/controller/GatewayController.java
@@ -0,0 +1,11 @@
+package com.projects.airline_gateway_svc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class GatewayController {
+    
+
+}
